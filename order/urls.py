66c3e9b822b080ab686b148e8ac5a30a6466f4fc
anand <anand24bala@gmail.com>
2025-06-28 01:35:45 +0800

    Initial commit

---
+++ order/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("menu/", views.menu, name="menu"),
+    path("menu/<slug:slug>/", views.menu_item, name="menu_item"),
+    # API Routes
+    path("api/menu/", views.api_menu, name="api_menu"),
+    path("api/menu-item/<slug:slug>/", views.item, name="item")
+]
