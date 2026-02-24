7f95181a4e954aa0abe15e2fc7770ff3ade47ed1
anand <anand24bala@gmail.com>
2026-02-24 13:03:31 +0800

    final fixes

--- frontend/src/services/menuServices.js
+++ frontend/src/services/menuServices.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000"
 
 export const getMenuItems = (business_slug) => {
-    return axios.get(`${BASE_URL}${business_slug}/api/menu/`);
+    return axios.get(`${BASE_URL}/${business_slug}/api/menu/`);
 };
 
 export const getMenuItemBySlug = (slug, business_slug) => {
-    return axios.get(`${BASE_URL}${business_slug}/api/menu-item/${slug}/`);
+    return axios.get(`${BASE_URL}/${business_slug}/api/menu-item/${slug}/`);
 };
 
